dc9d7a1ecf4a33701e40e28902a4cd37a0c449d7
626755343-alt <626755343@qq.com>
2026-09-20 12:23:59 +0800

    Add Vlog prompt interactions

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import { promptCatalog, selectPrompt } from './prompts.js';
+import { renderPrompt } from './renderPrompt.js';
+
+const promptContent = document.querySelector('#prompt-content');
+const refreshButton = document.querySelector('#refresh-prompt');
+
+let currentPrompt = selectPrompt(promptCatalog, null);
+
+function updatePrompt() {
+  currentPrompt = selectPrompt(promptCatalog, currentPrompt);
+  promptContent.classList.remove('is-refreshing');
+  void promptContent.offsetWidth;
+  promptContent.innerHTML = renderPrompt(currentPrompt);
+  promptContent.classList.add('is-refreshing');
+}
+
+promptContent.innerHTML = renderPrompt(currentPrompt);
+refreshButton.addEventListener('click', updatePrompt);
+
